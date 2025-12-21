d5a1fe57a670581a2361ffc938cb7dd9d4876bc0
Sagar Dhadke <peaceunique2003@gmail.com>
2025-12-21 11:37:42 +0530

    Added more colors for light and dart mode

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -2,13 +2,79 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color scaffoldDark = Color(0XFF1B1A1A);
-  static const Color primaryColor = Color(0xFF00B8D4);
-  static const Color errorColor = Color(0xFFFC8F8F);
-  static const Color white = Colors.white;
   static const Color grey10 = Color(0XFFA4A3A3);
   static const Color grey20 = Color(0XFF494848);
   static const Color grey40 = Color(0XFF323131);
   static const Color grey60 = Color(0XFF292828);
   static const Color grey80 = Color(0XFF1B1A1A);
   static const Color grey100 = Color(0XFF000000);
+
+  static const Color primaryColor = Color(0xFF00B8D4);
+  static const Color primaryLight = Color(0xFF62EFFF);
+  static const Color primaryDark = Color(0xFF0088A3);
+
+  static const Color errorColor = Color(0xFFFC8F8F);
+  static const Color errorLight = Color(0xFFFFBFBF);
+  static const Color errorDark = Color(0xFFC75F5F);
+
+  static const Color successColor = Color(0xFF4CAF50);
+  static const Color successLight = Color(0xFF80E27E);
+  static const Color successDark = Color(0xFF087F23);
+
+  static const Color warningColor = Color(0xFFFF9800);
+  static const Color warningLight = Color(0xFFFFB74D);
+  static const Color warningDark = Color(0xFFF57C00);
+
+  static const Color darkScaffold = Color(0XFF1B1A1A);
+  static const Color darkBackground = Color(0XFF121212);
+  static const Color darkSurface = Color(0XFF1E1E1E);
+  static const Color darkCard = Color(0XFF252525);
+
+  static const Color darkGrey10 = Color(0XFFA4A3A3);
+  static const Color darkGrey20 = Color(0XFF494848);
+  static const Color darkGrey40 = Color(0XFF323131);
+  static const Color darkGrey60 = Color(0XFF292828);
+  static const Color darkGrey80 = Color(0XFF1B1A1A);
+  static const Color darkGrey100 = Color(0XFF000000);
+
+  static const Color darkTextPrimary = Color(0xFFFFFFFF);
+  static const Color darkTextSecondary = Color(0xFFB0B0B0);
+  static const Color darkTextTertiary = Color(0xFF707070);
+  static const Color darkTextDisabled = Color(0xFF505050);
+
+  static const Color lightScaffold = Color(0xFFF5F5F5);
+  static const Color lightBackground = Color(0xFFFFFFFF);
+  static const Color lightSurface = Color(0xFFFAFAFA);
+  static const Color lightCard = Color(0xFFFFFFFF);
+
+  static const Color lightGrey10 = Color(0xFFF5F5F5);
+  static const Color lightGrey20 = Color(0xFFEEEEEE);
+  static const Color lightGrey40 = Color(0xFFE0E0E0);
+  static const Color lightGrey60 = Color(0xFFBDBDBD);
+  static const Color lightGrey80 = Color(0xFF9E9E9E);
+  static const Color lightGrey100 = Color(0xFF757575);
+
+  static const Color lightTextPrimary = Color(0xFF212121);
+  static const Color lightTextSecondary = Color(0xFF757575);
+  static const Color lightTextTertiary = Color(0xFF9E9E9E);
+  static const Color lightTextDisabled = Color(0xFFBDBDBD);
+
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color transparent = Colors.transparent;
+
+  static const Color darkDivider = Color(0xFF2C2C2C);
+  static const Color lightDivider = Color(0xFFE0E0E0);
+
+  static const Color darkShadow = Color(0x40000000);
+  static const Color lightShadow = Color(0x1A000000);
+
+  static const Color darkOverlay = Color(0x99000000);
+  static const Color lightOverlay = Color(0x66000000);
+
+  static const List<Color> primaryGradient = [primaryColor, primaryLight];
+
+  static const List<Color> darkGradient = [darkGrey60, darkGrey40];
+
+  static const List<Color> lightGradient = [lightGrey10, lightGrey20];
 }
