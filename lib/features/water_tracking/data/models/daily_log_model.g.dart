1afef153bb7ad4e18b0b61fef7c546171840acba
Sagar Dhadke <peaceunique2003@gmail.com>
2025-12-26 13:40:43 +0530

    Fixed BUg: Water tracking hive adapter model

--- lib/features/water_tracking/data/models/daily_log_model.g.dart
+++ lib/features/water_tracking/data/models/daily_log_model.g.dart
@@ -16,12 +16,26 @@ class DailyLogModelAdapter extends TypeAdapter<DailyLogModel> {
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return DailyLogModel();
+    return DailyLogModel(
+      date: fields[0] as DateTime,
+      intakes: (fields[1] as List).cast<WaterIntakeModel>(),
+      dailyGoal: fields[2] as double,
+      totalIntake: fields[3] as double,
+    );
   }
 
   @override
   void write(BinaryWriter writer, DailyLogModel obj) {
-    writer.writeByte(0);
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.date)
+      ..writeByte(1)
+      ..write(obj.intakes)
+      ..writeByte(2)
+      ..write(obj.dailyGoal)
+      ..writeByte(3)
+      ..write(obj.totalIntake);
   }
 
   @override
--- lib/features/water_tracking/data/models/water_intake_model.g.dart
+++ lib/features/water_tracking/data/models/water_intake_model.g.dart
@@ -16,12 +16,26 @@ class WaterIntakeModelAdapter extends TypeAdapter<WaterIntakeModel> {
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return WaterIntakeModel();
+    return WaterIntakeModel(
+      id: fields[0] as String,
+      amount: fields[1] as double,
+      timestamp: fields[2] as DateTime,
+      notes: fields[3] as String?,
+    );
   }
 
   @override
   void write(BinaryWriter writer, WaterIntakeModel obj) {
-    writer.writeByte(0);
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.amount)
+      ..writeByte(2)
+      ..write(obj.timestamp)
+      ..writeByte(3)
+      ..write(obj.notes);
   }
 
   @override
